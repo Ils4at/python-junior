4685725bb411289912948b46c0eabe28b06a1840
Ils4at <sharipov_3000@bk.ru>
2024-09-12 20:29:32 +0300

    work with Class and child Class

--- block_1/methods/task_2/implementation.py
+++ block_1/methods/task_2/implementation.py
@@ -1,11 +1,45 @@
+from block_1.common import MyException
+
+
 class ClassFather:
-    pass
+    registered_list = []
+
+    @staticmethod
+    def register():
+        raise MyException()
+
+    @staticmethod
+    def get_name():
+        raise MyException()
 
 
 class User1(ClassFather):
-    _name = ''
+    _name = ' '
+
+    @staticmethod
+    def register():
+        ClassFather.registered_list.append(1)
+
+    @staticmethod
+    def get_name():
+        if 1 in ClassFather.registered_list:
+            return User1._name
+        else:
+            raise MyException()
 
 
 class User2(ClassFather):
-    _name = ''
+    _name = ' '
+
+    @staticmethod
+    def register():
+        ClassFather.registered_list.append(2)
+
+    @staticmethod
+    def get_name():
+        if 2 in ClassFather.registered_list:
+            return User2._name
+        else:
+            raise MyException()
+
 
