9b06cdf7535ef53fe28a5746863e4ab1d08a0f98
Ils4at <90194596+Ils4at@users.noreply.github.com>
2024-09-13 08:41:55 +0300

    Update implementation.py
    First task

--- block_1/methods/task_1/implementation.py
+++ block_1/methods/task_1/implementation.py
@@ -1,2 +1,21 @@
 class Coffee:
-    pass
+    def __init__(self, name, ing1, ing2, ing3):
+        self.name = name
+        self.ing1 = ing1
+        self.ing2 = ing2
+        self.ing3 = ing3
+
+    def ingredients(self):
+        return f'В кофе {self.name} есть следующие ингредиенты: {self.ing1}, {self.ing2}, {self.ing3}\n'
+
+    def __str__(self):
+        return f'В кофе {self.name} есть следующие ингредиенты: {self.ing1}, {self.ing2}, {self.ing3}\n'
+
+
+late = Coffee('Late', 'milk', 'coffee', 'milk foam')
+cappuccino = Coffee('Cappuccino', 'coffe', 'water', 'coffe')
+glace = Coffee('Glace', 'coffe', 'milk', 'ice-cream')
+# late.ingredients()
+# cappuccino.ingredients()
+# glace.ingredients()
+print(late, cappuccino, glace)
