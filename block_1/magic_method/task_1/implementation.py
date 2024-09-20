5622c09ee1de01afe6e9b9353d9e2d65aaa369f4
Ильшат Шарипов Р. <ils.sharipov@bars.bars-kazan.ru>
2024-09-20 09:03:40 +0300

    magic_method

--- block_1/magic_method/task_1/implementation.py
+++ block_1/magic_method/task_1/implementation.py
@@ -1,24 +1,37 @@
 class Multiplier:
-    def __init__(self):
-        pass
+    def __init__(self, num):
+        self.num = num
+
+    def __add__(self, other):
+        return Multiplier(self.num + other.num)
+
+    def __sub__(self, other):
+        return Multiplier(self.num - other.num)
+
+    def __mul__(self, other):
+        return Multiplier(self.num * other.num)
+
+    def __truediv__(self, other):
+        return Multiplier(self.num / other.num)
+
+    def get_value(self):
+        return self.num
 
 
 class Hundred(Multiplier):
     """Множитель на 100"""
-    def __init__(self):
-        pass
+    def __init__(self, num):
+        self.num = num * 100
 
 
 class Thousand(Multiplier):
     """Множитель на 1 000"""
-    def __init__(self):
-        pass
+    def __init__(self, num):
+        self.num = num * 1000
 
 
 class Million(Multiplier):
     """Множитель на 1 000 000"""
-    def __init__(self):
-        pass
-
-
+    def __init__(self, num):
+        self.num = num * 1000000
 
